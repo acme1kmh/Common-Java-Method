fff554c9ee1d35029305349216d1415969925553
acme1kmh <acme1kmh@naver.com>
2020-07-10 13:32:13 +0900

    Create dateFormatUtil.java
    date Util 생성

---
+++ dateFormatUtil.java
@@ -0,0 +1,17 @@
+package com.lguplus.vr.admin.util;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class dateFormatUtil {
+
+	
+	/**
+	 * String 형식 date format 입력 시 호출 시점의 날짜 생성
+	 * @param dateFormat
+	 * @return
+	 */
+	public static String getDate(String dateFormat){
+		return new SimpleDateFormat(dateFormat).format(new Date()).toString();
+	}
+}
